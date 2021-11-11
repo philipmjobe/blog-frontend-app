ee032884ec5543ee26c6805c7403d5badfa8439e
Philip <philipmjobe@gmail.com>
2021-11-11 14:00:31 -0500

    added sign up, log in, log out and associated routes and imports

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Test from "../views/Test.vue";
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
 
 Vue.use(VueRouter);
 
@@ -24,6 +27,9 @@ const routes = [
     name: "Test",
     component: Test,
   },
+  { path: "/signup", name: "signup", component: Signup },
+  { path: "/login", name: "login", component: Login },
+  { path: "/logout", name: "logout", component: Logout },
 ];
 
 const router = new VueRouter({
